09e5de63340e0991267755f4ab99a28e2f64cd4e
Pratham <prathamjha18@gmail.com>
2024-08-14 21:36:14 +0530

    HI

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000';
+const API_URL = 'https://filesharing-backend-wk2j.onrender.com';
+
+
+
 export const uploadFile = async (data) => {
     try{
        let response = await axios.post(`${API_URL}/upload` , data);
